ad3031aa82e3116ffb414435af7a29cde7e9ab2c
LuanPM284 <mluanpatrick@gmail.com>
2024-06-19 14:46:35 +0200

    added an idea for step 7

---
+++ src/js/script.js
@@ -0,0 +1,12 @@
+// Step 7 - Multiplier table
+// This allows us to keep count of the times a certain multiplier was purchased
+const multiplierTableCounter = {
+    "2x": 0,
+    "5x": 0,
+    "10x": 0,
+    "100x": 0,
+}
+console.log(multiplierTableCounter["2x"]) // output: 0
+// multiplierTableCounter["2"] = 10;
+// console.log(multiplierTableCounter["2x"]) // output: 10
+
