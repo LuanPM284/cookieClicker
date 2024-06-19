e59b1689fd1ade834156820b7a22a43e6ae1c05f
LuanPM284 <mluanpatrick@gmail.com>
2024-06-19 15:17:05 +0200

    new code and conflict resolution

--- src/js/script.js
+++ src/js/script.js
@@ -1,17 +1,55 @@
 // Step 1 - base structure
+// veriables
+var pointsCounted = 1;
+var creditCounted = 1;
+var cookieClick = 1;
+
+let pointsTag = document.getElementById("points")
+let creditTag = document.querySelector("#credit");
+
+let coockieBtn = document.getElementById("coockie");
+coockieBtn.addEventListener("click", () => {
+    pointsTag.innerHTML = `Points: ${cookieClick}`;
+    creditTag.innerHTML = `Credit: ${creditCounted}`;
+    console.log(cookieClick + " " + creditCounted + " " + pointsCounted);
+    cookieClick++;
+    creditCounted++;
+    pointsCounted++;
+})
+// function incrementFaction() {
+//     return pointsCounted++;
+// }
+
+// let cookieBtn = document.getElementById("coockie");
+// cookieBtn.addEventListener("click", () => {
+//     incrementFaction();
+//   pointsCounted = cookieClick + pointsCounted;
+//   creditCounted = cookieClick + creditCounted;
+
+// pointsTag.textContent = `Points : ${pointsCounted}`;
+// creditTag.textContent = `Credit : ${creditCounted}`;
+
+//   return creditCounted;?
+//   console.log(creditCounted);
+// });
+
+// console.log("asdasd" + pointsCounted);
+// ===============================================================================
 // Step 2 -
+
+// ===============================================================================
 // Step 3 -
 // Step 4 -
 // Step 5 -
 // Step 6 -
 // Step 7 - Multiplier table
-const multiplierTableCounter = {
-  "2x": 0,
-  "5x": 0,
-  "10x": 0,
-  "100x": 0,
-};
-console.log(multiplierTableCounter["2x"]); // output: 0
+// const multiplierTableCounter = {
+//     "2x": 0,
+//     "5x": 0,
+//     "10x": 0,
+//     "100x": 0,
+// };
+// console.log(multiplierTableCounter["2x"]); // output: 0
 // multiplierTableCounter["2"] = 10;
 // console.log(multiplierTableCounter["2x"]) // output: 10
 // Step 8 -
@@ -21,29 +59,3 @@ console.log(multiplierTableCounter["2x"]); // output: 0
 // Step 12 -
 // This allows us to keep count of the times a certain multiplier was purchased
 
-// veriables
-var pointsCounted = 1;
-var creditCounted = 1;
-var cookieClick = 1;
-
-let pointsTag = document.querySelector("#points");
-let creditTag = document.querySelector("#credit");
-
-function incrementFaction() {
-  return pointsCounted++;
-}
-
-let cookieBtn = document.getElementById("coockie");
-cookieBtn.addEventListener("click", () => {
-  incrementFaction();
-  //   pointsCounted = cookieClick + pointsCounted;
-  //   creditCounted = cookieClick + creditCounted;
-
-  pointsTag.textContent = `Points : ${pointsCounted}`;
-  creditTag.textContent = `Credit : ${creditCounted}`;
-
-  //   return creditCounted;?
-  //   console.log(creditCounted);
-});
-
-console.log("asdasd" + pointsCounted);
