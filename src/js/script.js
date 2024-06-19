afc3f7705177e3634ea49119ad00e3bcc54766d5
LuanPM284 <mluanpatrick@gmail.com>
2024-06-19 16:16:59 +0200

    update on all

--- src/js/script.js
+++ src/js/script.js
@@ -1,48 +1,24 @@
 // Step 1 - base structure
-// veriables
-var pointsCounted = 1;
-var creditCounted = 1;
-var cookieClick = 1;
+// variables
 
-let pointsTag = document.getElementById("points")
-let creditTag = document.querySelector("#credit");
-
-let coockieBtn = document.getElementById("coockie");
-coockieBtn.addEventListener("click", () => {
-    pointsTag.innerHTML = `Points: ${cookieClick}`;
-    creditTag.innerHTML = `Credit: ${creditCounted}`;
-    console.log(cookieClick + " " + creditCounted + " " + pointsCounted);
-    cookieClick++;
-    creditCounted++;
-    pointsCounted++;
-})
-// function incrementFaction() {
-//     return pointsCounted++;
-// }
-
-// let cookieBtn = document.getElementById("coockie");
-// cookieBtn.addEventListener("click", () => {
-//     incrementFaction();
-//   pointsCounted = cookieClick + pointsCounted;
-//   creditCounted = cookieClick + creditCounted;
-
-// pointsTag.textContent = `Points : ${pointsCounted}`;
-// creditTag.textContent = `Credit : ${creditCounted}`;
-
-//   return creditCounted;?
-//   console.log(creditCounted);
-// });
-
-// console.log("asdasd" + pointsCounted);
 // ===============================================================================
 // Step 2 -
 
 // ===============================================================================
 // Step 3 -
+
+// ===============================================================================
 // Step 4 -
+
+// ===============================================================================
 // Step 5 -
+
+// ===============================================================================
 // Step 6 -
+
+// ===============================================================================
 // Step 7 - Multiplier table
+
 // const multiplierTableCounter = {
 //     "2x": 0,
 //     "5x": 0,
@@ -52,10 +28,21 @@ coockieBtn.addEventListener("click", () => {
 // console.log(multiplierTableCounter["2x"]); // output: 0
 // multiplierTableCounter["2"] = 10;
 // console.log(multiplierTableCounter["2x"]) // output: 10
+// ===============================================================================
 // Step 8 -
+
+// ===============================================================================
 // Step 9 -
+
+// ===============================================================================
 // Step 10 -
+
+// ===============================================================================
 // Step 11 -
+
+// ===============================================================================
 // Step 12 -
+
+// ===============================================================================
 // This allows us to keep count of the times a certain multiplier was purchased
 
