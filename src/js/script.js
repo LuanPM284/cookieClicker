6ca0b25f21c5224c3fe12e684be604cc6d5300af
anthosaxe <71581877+anthosaxe@users.noreply.github.com>
2024-06-22 17:29:10 +0200

    bonus button work

--- src/js/script.js
+++ src/js/script.js
@@ -145,6 +145,10 @@ function buybonus() {
     multiplierTaggle();
     endTime = Math.floor(Date.now() / 1000) + countdownDuration;
     setInterval(updateCountdown, 1000);
+    clickValue *= 2;
+    setTimeout(() => {
+      clickValue /= 2;
+    }, 30000);
   }
 }
 
@@ -186,7 +190,51 @@ times100xpoint.addEventListener("click", () => {
 
 });
 
-bonus.addEventListener('click', buybonus);
+bonus.addEventListener('click', () => {
+  buybonus();
+  times2xpoint.addEventListener("click", () => {
+    if (credit >= times2xpointPrice) {
+      applyMultiplier(multiplier2x*2, times2xpointPrice, times2xpoint, times2xzoneprice);
+      times2xpointPrice += 5;
+      times2xzoneprice.innerHTML = times2xpointPrice + "C";
+    }
+    multiplierTaggle();
+  });
+  
+  times5xpoint.addEventListener("click", () => {
+    if (credit >= times5xpointPrice) {
+      applyMultiplier(multiplier5x*2, times5xpointPrice, times5xpoint, times5xzoneprice);
+      times5xpointPrice += 10;
+      times5xzoneprice.innerHTML = times5xpointPrice + "C";
+    }
+    multiplierTaggle();
+  });
+  
+  times10xpoint.addEventListener("click", () => {
+    if (credit >= times10xpointPrice) {
+      applyMultiplier(multiplier10x*2, times10xpointPrice, times10xpoint, times10xzoneprice);
+      times10xpointPrice += 30;
+      times10xzoneprice.innerHTML = times10xpointPrice + "C";
+    }
+    multiplierTaggle();
+  });
+  
+  times100xpoint.addEventListener("click", () => {
+    if (credit >= times100xpointPrice) {
+      applyMultiplier(multiplier100x*2, times100xpointPrice, times100xpoint, times100xzoneprice);
+      times100xpointPrice += 200;
+      times100xzoneprice.innerHTML = times100xpointPrice + "C";
+    }
+    multiplierTaggle();
+  });
+
+  console.log(clickValue)
+
+  setTimeout(() => {
+    clickValue /= 2;
+    multiBox.innerHTML = "Multiplier : <br>" + clickValue;
+  }, 30000);
+});
 
 autoclic.addEventListener("click", buyAutoClicker);
 
